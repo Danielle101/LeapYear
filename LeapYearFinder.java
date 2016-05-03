960d8e668d21fd8869af1c378b09e3331725495f
DanielleHall <halldan4@yahoo.com>
2016-05-03 09:54:06 -0400

    first commit

---
+++ LeapYearFinder.java
@@ -0,0 +1,14 @@
+//This program will determine if the year a user inputs is a leap year or not
+package leapYearFinder;
+
+public class LeapYearFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to the leap year finder program!\n\nWhen you enter a"
+		+ " year the program will tell you if the year was or will be a leap year or"
+		+ " not.Let's begin!\n");
+	System.out.println("Please enter a year(yyyy):");
+	}
+
+}
